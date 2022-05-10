e70d958621bacfe59b389b0713a4f441f55f007b
James Wafula <wafulajames9@gmail.com>
2022-05-10 15:21:38 +0300

    Authentication(Signup):Input form - Created the input fields - Added validatios to input fields - Added buttons and made them reusabele components [Deliver]: #636541

--- ka-demo/pages/index.js
+++ ka-demo/pages/index.js
@@ -21,8 +21,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Irrelevant</h2>
           </a>
 
           <a href='https://nextjs.org/learn' className={styles.card}>
